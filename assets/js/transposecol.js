26be13f3e3337c099e866cff58f6423a36af4651
Marc Morcos <marc.morcos1@hotmail.com>
2024-08-07 00:02:42 -0400

    adaptive row transpose

--- assets/js/transposecol.js
+++ assets/js/transposecol.js
@@ -1,10 +1,29 @@
 // Take 1d array that would be split into 3 cols and transpose it
 document.addEventListener("DOMContentLoaded", function() {
     const postList = document.getElementById('post-list');
+
+    //get number of columns
+    let numColumns = 1;
+    try{
+        const element = document.getElementById('post-container'),
+            style = window.getComputedStyle(element),
+            count = style.getPropertyValue('column-count');
+            if(!isNaN(count)){
+                numColumns = count;
+            }
+    }catch(e) {
+        console.log(e);
+    }
+
+    //no need to do anything if one column
+    if(numColumns == 1){
+        return;
+    }
+    
     const posts = Array.from(postList.getElementsByClassName('post-item'));
-    const numColumns = 3;
+    
     const numRows = Math.ceil(posts.length / numColumns);
-  
+
     // Create an array to hold the transposed order
     const transposedPosts = new Array(posts.length);
   
